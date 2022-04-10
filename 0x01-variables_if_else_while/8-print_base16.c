bd3ac4f2aaefb92cbe60289d0d07a7a27d2906d2
temmytope88 <fakiletemitope@gmail.com>
2022-04-10 11:17:59 +0100

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
