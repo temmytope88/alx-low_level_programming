4144f8a5d0c1265c94113adf094506b889caf26e
temmytope88 <fakiletemitope@gmail.com>
2022-04-10 02:39:04 +0000

    remainder of 10 less or greater than 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *  
+ *  Return: Always O (Success)
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int x = n%10;
+	
+	if (x > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	}
+	else if (x == 0)
+	{
+		prinf("Last digit of %d is %d and is zero\n");
+	
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 but not 0\n");
+	}
+		return (0);
+}
