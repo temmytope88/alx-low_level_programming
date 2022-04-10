3027524a5d9e4de75713b27585ebf48bcfd319b8
temmytope88 <fakiletemitope@gmail.com>
2022-04-10 09:05:10 +0100

    alpha reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char g = 'z';
+	for(g = 'z'; g <= 'a'; z--)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+	return (0);
+}
