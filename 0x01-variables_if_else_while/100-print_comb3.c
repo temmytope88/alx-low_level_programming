e6b41f86ef310963f690dbb30e0399e61ae2de0d
temmytope88 <fakiletemitope@gmail.com>
2022-04-10 16:12:47 +0100

    double digit from 0-9 without reoccurence

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 ( Success)
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for(a = 48; a <= 57; a++)
+	{
+		for(b = 48; b <= 57; b++)
+		{
+			if(a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if(a < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+}
