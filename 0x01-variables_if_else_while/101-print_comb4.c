6e10ac402ffe52d0a4354dd5cb19fce1c15db3cd
temmytope88 <fakiletemitope@gmail.com>
2022-04-12 03:27:33 +0100

    3 digits of numbers from 0-9 non-repeating

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,19 +3,19 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
-int main() 
+int main(void)
 {
 	int a;
 	int b;
 	int c;
     
-	for(a = 48; a <= 57; a++)
+	for (a = 48; a <= 57; a++)
 	{
-	for(b = 48; b <= 57; b++)
+	for (b = 48; b <= 57; b++)
 	{
-		if(a != b && a < b)
+		if (a != b && a < b)
 		{
-			for(c = 48; c <= 57; c++)
+			for (c = 48; c <= 57; c++)
 			{
 				if (c != b && b < c)
 				{
@@ -33,5 +33,5 @@ int main()
 	}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
