b437daf80b6d204b0cdd3ad4660e7ee71bd3a551
temmytope88 <62169700+temmytope88@users.noreply.github.com>
2022-04-14 02:55:03 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_×10 - prints the alphabet in lowercase 10 times.
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
  *
  * Return: Always 0.
- **/
+ */
 void print_alphabet_x10(void)
 {
 	char c;
