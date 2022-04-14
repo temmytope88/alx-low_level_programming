a60b23d580ba312d135e7c133b997717db65611e
temmytope88 <fakiletemitope@gmail.com>
2022-04-14 02:00:31 +0100

    correct code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,12 @@
 void print_alphabet(void)
 {
 	char c;
+	int i
+	for (i = 0; i <= 9; i++)
+	{
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-	_putchar('\n');
-	return (0);
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
 }
