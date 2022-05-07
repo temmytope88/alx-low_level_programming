a0ac586c7f00e7b972adb149de39abf0e70bb0fe
temmytope88 <fakiletemitope@gmail.com>
2022-05-07 17:38:51 +0100

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,4 +19,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+vaagra
