b561e2281e9e25d431fd8ee27164d12e5d06ecb9
temmytope88 <fakiletemitope@gmail.com>
2022-05-14 03:14:29 +0000

    betty compliance

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
 /**
-* for initialiation of the struct dog
-*/
+** init_dog - Initializes a variable of type struct dog
+**@d: Dog object
+**@name: Name of dog
+**@age: Age of dog
+**@owner: Owner of dog
+*
+*Return: Always 0.
+**/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
