6ce4cad98705c084d069922744d9b881682814cd
temmytope88 <fakiletemitope@gmail.com>
2022-05-17 16:40:47 +0100

    betty compliance

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-/** 
- * main - print the number of arguments passed to the program 
- * @argc: number of arguments 
- * @argv: array of arguments 
- *
- * Return: Always 0 (Success) 
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", arg-1);
 	return (0);
 }
