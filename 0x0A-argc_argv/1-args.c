e750e38ab8e534e85edcfa611eb68d97630ff485
temmytope88 <fakiletemitope@gmail.com>
2022-05-17 16:26:31 +0100

    corrrect

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-int main(int argc, char *argv[] _attribute_((unused)) )
+int main(int argc, char *argv[]_attribute_((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
