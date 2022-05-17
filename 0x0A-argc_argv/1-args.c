7fe98829f68dd276f12996c651742d7c18fc3f8e
temmytope88 <fakiletemitope@gmail.com>
2022-05-17 15:57:24 +0100

    counting number of arguments passed to a commandline

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+learning about argc and argv when argument is passed to a command line.
