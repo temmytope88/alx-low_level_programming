82018892e891459105567019f5bdd27f3f97d29f
temmytope88 <fakiletemitope@gmail.com>
2022-05-18 10:50:03 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
 {
 	int i, sum;
 	sum = 0;
-	if(argc == 1)
+	if (argc == 1)
 		printf("0\n");
 	else
-		for(i = 1; i < argc; i++)
-			if(!isdigit(atoi(argv[i])) || atoi(argv[i]) < 0)
+		for (i = 1; i < argc; i++)
+			if (!isdigit(atoi(argv[i])) || atoi(argv[i]) < 0)
 				return (1);
 			else
 				sum = sum + atoi(argv[i]);
