bfc996fbbdb3f549e3a0c6e4c0d877624c103c10
temmytope88 <fakiletemitope@gmail.com>
2022-05-29 03:20:01 +0100

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, size;
+	char *a;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	size = i + j + 1;
+	a = malloc(sizeof(char) * size);
+	if (a == 0)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		*(a + i) = *(s1 + i);
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		*(a + i + j) = *(s2 + j);
+	}
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **twoD, x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	twoD = malloc(sizeof(int *) * height);
+	if (twoD == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		twoD[x] = malloc(sizeof(int) * width);
+		if (twoD[x] == NULL)
+		{
+			for ( ; x >= 0; x--)
+			{
+				free(twoD[x]);
+			}
+		free(twoD);
+		return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			twoD[x][y] = 0;
+	}
+	return (twoD);
+}
